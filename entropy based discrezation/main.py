c76feef09ee406406e247d216654397180a747ca
shuivin <shuivin@gmail.com>
2012-11-08 01:02:34 +0800

    Source code for Beginning Android Application Development的源代碼
    老師要做一個android課業項目，只好拿這本書學習了

--- entropy based discrezation/main.py
+++ entropy based discrezation/main.py
@@ -18,17 +18,9 @@ plt.xlim(0, len(class_dict)+1)
 plt.ylim(0,4)
 #x = np.linspace(0, 15, len(class_list))
 plt.plot(class_dict.keys(),class_dict.values(), "gp")
-#x = plt.arange(100)
-#plt.plot(10*plt.ones(100), x , 'ro', label = 'x = 10')
+x = plt.arange(100)
+##plt.plot(20*plt.ones(100), x , 'ro', label = 'x = 10')
 plt.show()
-def fuc_purity():
-	numerical_list = class_dict.keys()
-	#categorical_list = class_dict.values()
-	#比較每個點作為分割點的純度
-	for split_point in numerical_list[1:]:
-		#split_point 為分割點
-		categorical_a = 0, categorical_b = 0, categorical_c = 0
-		
-	pass
+
 
 
